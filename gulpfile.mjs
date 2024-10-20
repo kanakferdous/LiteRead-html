8575b8cbcef0abf623618f3b8479b104e8d50967
kanakferdous <kanakferdous@gmail.com>
2024-10-21 01:03:41 +0600

    Header and Top Bar Updated

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,24 +1,24 @@
 import gulp from "gulp";
 import autoPrefixer from "gulp-autoprefixer";
 import concat from "gulp-concat";
-import gulpSass from "gulp-sass";
+import gulpSass from "gulp-sass"; // Import gulp-sass
 import uglify from "gulp-uglify";
-import dartSass from "sass";
+import dartSass from "sass"; // Import Dart Sass
 
-const sass = gulpSass(dartSass);
+const sass = gulpSass(dartSass); // Use Dart Sass with gulp-sass
 
 // Compile SCSS into CSS
 export function style() {
   return gulp
     .src("./src/scss/style.scss")
-    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError)) // Compile using dart-sass
     .pipe(
       autoPrefixer({
-        overrideBrowserslist: ["last 2 versions"], // Corrected here
+        overrideBrowserslist: ["last 2 versions"],
         cascade: false,
       })
     )
-    .pipe(gulp.dest("./dist/css"));
+    .pipe(gulp.dest("./dist/css")); // Output the compiled CSS to the dist folder
 }
 
 // Minify JS
